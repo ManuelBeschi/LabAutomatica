61e2ac6cf59ca85c54b4cd2fc7909ed1b7bb36fe
Manuel Beschi <manuel.beschi@unibs.it>
2022-03-10 14:55:55 +0100

    9 marzo

--- identificazione/SimpleScaraController.m
+++ identificazione/SimpleScaraController.m
@@ -5,13 +5,17 @@ classdef SimpleScaraController < BaseController
     end
     methods
         function u=computeControlAction(obj,reference,y)
+
+            % PD posizione del motore usando la coppia.
+            % u= Kp*errore_posizione + Kv*errore_velocità
             reference_position=reference(1:2);
             reference_velocity=reference(3:4);
             position=y(1:2);
             velocity=y(3:4);
             u=obj.Kp*(reference_position-position)+...
-                obj.Kp*(reference_velocity-velocity);
-            u=min(max(u,-obj.umax),obj.umax);
+                obj.Kv*(reference_velocity-velocity);
+
+            u=min(max(u,-obj.umax),obj.umax); % saturare
         end
     end
 end
